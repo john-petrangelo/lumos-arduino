4f873bd44f00fb67c62a44623641988c93bd37a6
John Petrangelo <>
2021-09-23 21:51:24 -0400

    Moved all Color definitions to Color.h

--- lumos-arduino/Colors.h
+++ lumos-arduino/Colors.h
@@ -3,6 +3,8 @@
 
 #include "defs.h"
 
+typedef uint32_t Color;
+
 class Colors {
   public:
     static Color blend(Color a, Color b, int ratio);
@@ -15,5 +17,17 @@ class Colors {
     static uint8_t getGreen(Color color);
     static uint8_t getBlue(Color color);
 };
+
+Color const BLACK =  0x000000;
+Color const BLUE =   0x0000FF;
+Color const GREEN =  0x00FF00;
+Color const CYAN =   0x00FFFF;
+Color const RED =    0xFF0000;
+Color const PURPLE = 0xFF00FF;
+Color const YELLOW = 0xFFFF00;
+Color const WHITE =  0xFFFFFF;
+Color const ORANGE = 0xFF7F00;
+Color const INDIGO = 0x4B0082;
+Color const VIOLET = 0x8F00FF;
 
 #endif // _COLORS_H_
--- lumos-arduino/defs.h
+++ lumos-arduino/defs.h
@@ -15,21 +15,8 @@ enum Direction {
 uint16_t const NUM_PIXELS = 60;
 typedef uint32_t Color;
 typedef Color* Pixels;
-typedef Color PixelsArray[60];
-
-Color const BLACK =  0x000000;
-Color const BLUE =   0x0000FF;
-Color const GREEN =  0x00FF00;
-Color const CYAN =   0x00FFFF;
-Color const RED =    0xFF0000;
-Color const PURPLE = 0xFF00FF;
-Color const YELLOW = 0xFFFF00;
-Color const WHITE =  0xFFFFFF;
-Color const ORANGE = 0xFF7F00;
-Color const INDIGO = 0x4B0082;
-Color const VIOLET = 0x8F00FF;
+typedef Color PixelsArray[NUM_PIXELS];
 
 extern Adafruit_NeoPixel strip;
 
 #endif // _DEFS_H_
-
